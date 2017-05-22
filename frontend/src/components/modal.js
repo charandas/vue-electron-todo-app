98208470ba92966617457aa69dfdcbbc53b249eb
Charandas Batra-Daitch <charandas@fathm.io>
2017-05-22 19:17:45 -0400

    Add esc and outside click to modal component

--- frontend/src/components/modal.js
+++ frontend/src/components/modal.js
@@ -4,7 +4,41 @@ import modalTpl from './modal.html!vtc';
 import './modal.css!css';
 
 const MyModal = Vue.component('my-modal', {
-  props: ['result'],
+  props: ['result', 'onOutsideClick'],
+  data () {
+    return {
+      overModal: true,
+      focused: false
+    };
+  },
+  methods: {
+    focusOn: function () {
+      this.focused = true;
+    },
+    onOutsideClickHandler: function (e) {
+      if (!this.overModal && this.focused) {
+        if (this.onOutsideClick) {
+          this.onOutsideClick();
+        } else {
+          this.result('cancel');
+        }
+      }
+    }
+  },
+  created () {
+    window.addEventListener('keyup', e => {
+      if (e.keyCode === 27) {
+        this.overModal = false;
+        this.onOutsideClickHandler();
+      }
+    });
+  },
+  mounted () {
+    document.addEventListener('click', this.onOutsideClickHandler);
+  },
+  beforeDestroy () {
+    document.removeEventListener('click', this.onOutsideClickHandler);
+  },
   render: modalTpl.render,
   staticRenderFns: modalTpl.staticRenderFns
 });

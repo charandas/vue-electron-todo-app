87c01e7aa0cb3dd08b48d12e576afcc701953261
Charandas Batra-Daitch <charandas@fathm.io>
2017-05-21 22:06:00 -0400

    Fix config path

--- src/config-lib/index.js
+++ src/config-lib/index.js
@@ -2,8 +2,5 @@ import path from 'path';
 
 import aceConfig from './config';
 
-// TODO: ugly hack but only for development
-const pathToConfig = process.argv[2] === 'development'
-  ? path.resolve(__dirname, '..', 'config')
-  : path.resolve(__dirname, '..', '..', 'config');
+const pathToConfig = path.resolve(__dirname, '..', 'config');
 export default aceConfig(pathToConfig);

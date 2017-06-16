f1c4121d95b3d2cb9b566c90e8e1160b3a606404
Charandas Batra-Daitch <charandas108@gmail.com>
2017-06-16 16:45:30 -0400

    Fix for production:

--- src/audio.js
+++ src/audio.js
@@ -5,9 +5,16 @@ import path from 'path';
 import uuidV4 from 'uuid/v4';
 import { BrowserWindow } from 'electron';
 
+import config from './config-lib/index';
 import { getLogger } from './app_ready';
 
 const logger = getLogger({ label: 'audio' });
+let ffmpegPath = ffmpegStatic.path;
+
+if (config.get('NODE_ENV') === 'production') {
+  ffmpegPath = ffmpegPath.replace('app.asar', 'app.asar.unpacked');
+}
+logger.silly(`ffmpeg from path: ${ffmpegPath}`);
 
 export function extractAudioFromVideo ({ movieUrl, taskId = uuidV4() }) {
   const dirname = path.dirname(movieUrl);
@@ -16,11 +23,9 @@ export function extractAudioFromVideo ({ movieUrl, taskId = uuidV4() }) {
   const outputUrl = path.resolve(dirname, `${filename}-audio.mp3`);
   const mainWindow = BrowserWindow.getFocusedWindow();
 
-  logger.silly(`ffmpeg from path: ${ffmpegStatic.path}`);
-
   return new Bluebird((resolve, reject) => {
     ffmpeg(movieUrl)
-      .setFfmpegPath(ffmpegStatic.path)
+      .setFfmpegPath(ffmpegPath)
       .audioBitrate('192k')
       .noVideo()
       .audioCodec('libmp3lame')
@@ -32,11 +37,15 @@ export function extractAudioFromVideo ({ movieUrl, taskId = uuidV4() }) {
       })
       .on('progress', progress => {
         mainWindow.webContents.send('extractAudioProgress', Object.assign({}, progress, { taskId, outputUrl, dirname }));
+        logger.info(progress);
         logger.info(`Processing: ${progress.percent}% done`);
       })
       .on('end', () => {
         logger.info(`Processing finished and produced ${outputUrl}.`);
         resolve({ processed: true });
+      })
+      .on('stderr', stderrLine => {
+        logger.error(`From ffmpeg stderr: ${stderrLine}`);
       })
       .save(outputUrl);
   });
